3f2402cb0a085c3ba3f75468c62bb7ece2ef2b75
ammar-coh <ammarkhn91@gmail.com>
2024-02-24 07:47:28 +0500

    sd

--- src/socket.js
+++ src/socket.js
@@ -5,8 +5,7 @@ const URL = process.env.REACT_APP_BASE_URL;
 
  export const socket = io(URL, {
     autoConnect: true,
-   
-    extraHeaders: {
+   extraHeaders: {
         'Access-Control-Allow-Origin': '*',
         // Additional headers if required
     },
