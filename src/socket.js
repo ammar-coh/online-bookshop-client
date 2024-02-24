6b9c2521dce5b28769727e53a0f3d57bceb847f9
ammar-coh <ammarkhn91@gmail.com>
2024-02-24 19:43:23 +0500

    socket

--- src/socket.js
+++ src/socket.js
@@ -5,9 +5,5 @@ const URL = process.env.REACT_APP_BASE_URL;
 
  export const socket = io(URL, {
     autoConnect: true,
-   extraHeaders: {
-        'Access-Control-Allow-Origin': '*',
-        // Additional headers if required
-    },
   });;
   // const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:8081';
